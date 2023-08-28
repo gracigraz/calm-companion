a868ce6c47958396bc0e667e8f2b85bddff5018f
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-08-27 23:20:00 -0400

    updated functionality hacks componenet to get all coping-strategies from database

--- src/components/Hacks/Hacks.js
+++ src/components/Hacks/Hacks.js
@@ -1,16 +1,47 @@
 import "./Hacks.scss";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase-config.js";
+import { getDocs, collection, doc } from "firebase/firestore";
+{
+}
 
 function Hacks() {
+  const [strategyList, setStrategyList] = useState([]);
+
+  const copingStrategiesCollectionRef = collection(db, "coping-strategies");
+
+  useEffect(() => {
+    const getStrategyList = async () => {
+      //Read the data
+      //Set the strategy list
+      try {
+        const data = await getDocs(copingStrategiesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })); //grab the data that we want from data some pieces of information, not really filtered
+        console.log(filteredData);
+        setStrategyList(filteredData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getStrategyList();
+  }, []);
+
   return (
     <>
       <div className="hacks">
         <div className="hacks__back"></div>
         <h4 className="hacks__title">
-          What are some relaxing spots to hang out that can help shift your
-          focus from your worries?
+          Activities you love that help take your mind off worries!
         </h4>
         <div className="hacks__container">
-          <button className="hacks-button"></button>
+          {strategyList.map((strategy) => (
+            <button key={doc.id} className="hacks__button">
+              {strategy.Name}
+            </button>
+          ))}
         </div>
         <button className="hacks__save">Save</button>
       </div>
--- src/components/Spots/Spots.js
+++ src/components/Spots/Spots.js
@@ -4,6 +4,10 @@ function Spots() {
   return (
     <>
       <div className="spots">
+        <h4 className="spots__title">
+          Your top hangout spots where you can chill and shift your focus from
+          worries!
+        </h4>
         <div className="spots__map" id="map"></div>
       </div>
     </>
