bbe2dc47f4f74af7309a9a6f519828524c07efe0
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-08-29 08:18:00 -0400

    started dashboard page

---
+++ src/pages/DasshboardPage/DashboardPage.js
@@ -0,0 +1,19 @@
+import "./DashboardPage.scss";
+
+function DashboardPage() {
+  return (
+    <>
+      <main className="not-found">
+        <div className="not-found__round-container">
+          <h1 className="not-found__error">404</h1>
+        </div>
+        <p className="not-found__message">
+          Can't find what you're looking for, sorry! Don't worry thought,
+          everything is STILL AWESOME
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default DashboardPage;
