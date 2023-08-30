2223c2a1a8521716c3ff806937f3186352eca09d
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-08-30 14:02:33 -0700

    updated CrisisPage to redirect urgent care lis tof facilities to a different page

--- src/pages/CrisisPage/CrisisPage.js
+++ src/pages/CrisisPage/CrisisPage.js
@@ -1,54 +1,8 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-
+import Nav from "../../components/Nav/Nav";
 import "./CrisisPage.scss";
 
-function MapboxLocations() {
-  const [locatiuns, setLocations] = useState([]);
-  const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_DEFAULT_ACCESS_TOKEN;
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.mapbox.com/search/searchbox/v1/suggest?q=urgent+care&language=en&proximity=-80.139198,25.793449&types=poi&session_token=0f4f2121-34e4-45a1-88a3-4476a1704c44&access_token=" +
-          ACCESS_TOKEN
-      )
-      .then((response) => {
-        // const fetchedLocations = response.data.features;
-        const fetchedLocations = response.data.suggestions;
-        console.log(response);
-        setLocations(fetchedLocations);
-      })
-      .catch((error) => {
-        console.error("An error occurred:", error);
-      });
-  }, []);
-
-  return (
-    <div className="mapbox-locations">
-      <h4>Urgent Care Locations</h4>
-      <div className="locations-list">
-        {locatiuns.map((locatiun, index) => (
-          <div key={index} className="location-item">
-            <h4> {locatiun.name}</h4>
-            <p>{locatiun.full_address}</p>
-          </div>
-          // {locations.map((location, index) => (
-          //   <div key={index} className="location-item">
-          //     <h4> {location.properties.name}</h4>
-          //     <p>{location.properties.full_address}</p>
-          //     <p>Latitude: {location.geometry.coordinates[1]}</p>
-          //     <p>Longitude: {location.geometry.coordinates[0]}</p>
-          //   </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
 function CrisisPage() {
-  const [showMapboxLocations, setShowMapboxLocations] = useState(false);
-
   const handleCallEmergency = () => {
     window.location.href = "tel:911";
   };
@@ -58,7 +12,7 @@ function CrisisPage() {
   };
 
   const handleFindUrgentCare = () => {
-    setShowMapboxLocations(true);
+    window.location.href = "/urgent-care";
   };
 
   const handleFindER = () => {
@@ -91,7 +45,7 @@ function CrisisPage() {
           </div>
         </div>
       </main>
-      {showMapboxLocations && <MapboxLocations />}
+      <Nav />
     </>
   );
 }
