ad6b27c33470d9fbc74e1195a1a0e2facd4ea748
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-09-06 10:14:39 -0400

    updated functionality of toolkit components in general

--- src/components/Spots/Spots.js
+++ src/components/Spots/Spots.js
@@ -2,8 +2,95 @@ import "./Spots.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { db } from "../../firebase-config";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 function Spots() {
+  const [spotName, setSpotName] = useState("");
+  const [spots, setSpots] = useState([]);
+  const loggedUser = localStorage.getItem("user");
+  const [userData, setUserData] = useState([]);
+  const conditionUser = userData === null ? true : false;
+
+  const fetchSpots = async () => {
+    try {
+      const spotsSnapshot = await getDocs(
+        collection(db, "users", loggedUser, "spots")
+      );
+
+      const fetchedSpots = [];
+      spotsSnapshot.forEach((spotDoc) => {
+        fetchedSpots.push({ id: spotDoc.id, ...spotDoc.data() });
+      });
+
+      setSpots(fetchedSpots);
+    } catch (error) {
+      console.error("Error fetching spots:", error);
+    }
+  };
+
+  const handleAddSpot = async () => {
+    try {
+      // Add the new spot to the user's collection
+      const userSpotsRef = collection(db, "users", loggedUser, "spots");
+      await addDoc(userSpotsRef, {
+        spotName,
+      });
+
+      // Fetch the updated spots and update the state
+      fetchSpots();
+
+      // Clear the input field
+      setSpotName("");
+    } catch (error) {
+      console.error("Error adding spot:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSpots();
+  }, []);
+
+  const handleDeleteSpot = async (spotId) => {
+    try {
+      const userSpotsRef = doc(db, "users", loggedUser, "spots", spotId);
+      await deleteDoc(userSpotsRef);
+
+      // Fetch the updated spots and update the state
+      fetchSpots();
+    } catch (error) {
+      console.error("Error deleting spot:", error);
+    }
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const snap = await doc(db, "users", loggedUser);
+        if (snap.exists()) {
+          let data = snap.data();
+          setUserData(data);
+        } else {
+          console.log("No such document");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUser();
+  }, [conditionUser]);
+
+  if (conditionUser) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div className="spots">
@@ -18,12 +105,29 @@ function Spots() {
             Your top hangout spots where you can chill and shift your focus from
             worries!
           </h3>
-          <div className="spots__map" id="map"></div>
 
-          <input className="spots__input" placeholder="Name" />
-          <button className="spots__add">+ Add Spot</button>
-          {/* button changes color when added */}
-          <button className="spots__delete">- Delete</button>
+          <input
+            className="spots__input"
+            placeholder="Name"
+            value={spotName}
+            onChange={(e) => setSpotName(e.target.value)}
+          />
+          <button className="spots__add" onClick={handleAddSpot}>
+            + Add Spot
+          </button>
+
+          <div className="spots__list">
+            {spots.map((spot) => (
+              <div key={spot.id} className="spots__spot">
+                {spot.spotName}
+                <FontAwesomeIcon
+                  className="fa-1x hacks__delete"
+                  icon={icon({ name: "trash", style: "solid" })}
+                  onClick={() => handleDeleteSpot(spot.id)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
